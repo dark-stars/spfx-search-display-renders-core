0a945b7dca271de61d481214feaf408edfd8a408
cl0wn3d <peter.l.vranich@outlook.com>
2019-07-21 20:36:03 +1000

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { RendererType } from "./models/RendererType";
-export { default as SearchResultsRendererService } from "./services/SearchResultsRendererService/SearchResultsRendererService";
+export { RendererType } from './models/RendererType';
+export {
+  default as SearchResultsRendererService,
+} from './services/SearchResultsRendererService/SearchResultsRendererService';
